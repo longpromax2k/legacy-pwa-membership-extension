1549d3ce497f256385cb321b3502ef9cb31948eb
long <78641196+tatsuk1i@users.noreply.github.com>
2022-05-23 09:30:41 +0700

    docs: add jsdoc

--- @simicart/memberships/src/components/items/benefitlist/benefitlist.js
+++ @simicart/memberships/src/components/items/benefitlist/benefitlist.js
@@ -4,6 +4,13 @@ import { shape, string } from 'prop-types';
 
 import defaultClasses from './benefitlist.module.css';
 
+/*
+* @summary - BenefitList component
+* @param {object} props - React props
+* @props {string} classes - Additional classes
+* @props {object} benefit - Benefit object
+* @return {ReactElement} - Rendered component
+*/
 const BenefitList = props => {
     const classes = mergeClasses(defaultClasses, props.classes);
     const parsedBenefits = JSON.parse(props.benefits);
